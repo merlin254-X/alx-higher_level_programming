e8c8dad426830b28ce49cb00bcb456d6a3e2de65
merlin254-X <merlinderick8@gmail.com>
2023-11-28 14:35:07 -0800

    task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
-    """Print numbers from 1 to 100, replacing multiples of 3 with Fizz, multiples of 5 with Buzz, and multiples of both with FizzBuzz."""
+    """Print numbers from 1 to 100, replacing multiples of 3 with Fizz,
+    multiples of 5 with Buzz, and multiples of both with FizzBuzz."""
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
