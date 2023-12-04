c95f9908835c0bd7d92acf64d91df51c30bd86ac
merlin254-X <merlinderick8@gmail.com>
2023-12-04 16:45:58 +0300

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 def element_at(my_list, idx):
-
+    # Check if idx is negative or out of range
     if idx < 0 or idx >= len(my_list):
-        return none
-
-    else:
-        return my_list[idx]
+        return None
+    # Return the element at the given index
+    return my_list[idx]
