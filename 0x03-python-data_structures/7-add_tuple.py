ab2d4ea778a9c87c072e5f695d99170d4f504864
merlin254-X <merlinderick8@gmail.com>
2023-12-04 16:26:41 +0300

    Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Extract the first two elements from each tuple (or use 0 if missing)
-    a1, a2 = tuple_a[:2] if len(tuple_a) >= 2 else (0, 0)
-    b1, b2 = tuple_b[:2] if len(tuple_b) >= 2 else (0, 0)
+    # Initialize the result tuple with two zeros
+    result = (0, 0)
 
-    # Perform addition and return a new tuple
-    result_tuple = (a1 + b1, a2 + b2)
-    return result_tuple
+    # Loop through the first two elements of each tuple
+    for i in range(2):
+        # Get the element from tuple_a or 0 if it does not exist
+        a = tuple_a[i] if i < len(tuple_a) else 0
+        # Get the element from tuple_b or 0 if it does not exist
+        b = tuple_b[i] if i < len(tuple_b) else 0
+        # Add the elements and store them in the result tuple
+        result = result[:i] + (a + b,) + result[i+1:]
+
+    # Return the result tuple
+    return result
