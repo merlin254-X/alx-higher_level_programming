78d58feba305adb216012be75aae236a1970bd2a
merlin254-X <merlinderick8@gmail.com>
2023-12-05 03:13:52 +0300

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,8 +1,5 @@
 #include "lists.h"
 
-listint_t *reverse_listint(listint_t **head);
-int is_palindrome(listint_t **head);
-
 /**
  * reverse_listint - Reverses a singly-linked listint_t list.
  * @head: A pointer to the starting node of the list to reverse.
@@ -70,7 +67,6 @@ int is_palindrome(listint_t **head)
 
 	return (1);
 }
-
 
 /**
  * free_listint - frees a listint_t list
