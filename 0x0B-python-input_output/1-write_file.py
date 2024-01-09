4e51c314c12f50bc1d42bd543b23fdb6616cf0cc
merlin254-X <merlinderick8@gmail>
2024-01-09 10:46:59 -0800

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and returns
+    the number of characters written.
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        """
+        Read the file content and print it to stdout.
+        """
+        nb_characters = file.write(text)
+    return nb_characters
