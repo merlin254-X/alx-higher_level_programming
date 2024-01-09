c32dc3b80fbd63485a46b5dbd665d0c04fc02f08
merlin254-X <merlinderick8@gmail>
2024-01-09 10:51:50 -0800

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+"""
+    1-write_file: write_file()
+"""
+
 
 def write_file(filename="", text=""):
     """
-    Writes a string to a text file (UTF8) and returns
-    the number of characters written.
+        write_file writes a string to a text file.
+        Args:
+            filename (str): name of file.
+            text (str): text to be written.
+        Returns: number of bytes written.
     """
-    with open(filename, 'w', encoding='utf-8') as file:
-        """
-        Read the file content and print it to stdout.
-        """
-        nb_characters = file.write(text)
-    return nb_characters
+    with open(filename, "w", encoding='utf-8') as a_file:
+        return a_file.write(text)
