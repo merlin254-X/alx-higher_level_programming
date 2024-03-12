493b25c57d2c87f2723e1fd84535c38d3b96ea9c
merlin254-X <merlinderick8@gmail.com>
2024-03-12 14:34:47 -0700

    all

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,19 +1,6 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-const sourceFile1 = process.argv[2];
-const sourceFile2 = process.argv[3];
-const destinationFile = process.argv[4];
-
-// Read content of source files
-const content1 = fs.readFileSync(sourceFile1, 'utf8');
-const content2 = fs.readFileSync(sourceFile2, 'utf8');
-
-// Concatenate content
-const concatenatedContent = content1 + content2;
-
-// Write concatenated content to destination file
-fs.writeFileSync(destinationFile, concatenatedContent);
-
-console.log(`Files ${sourceFile1} and ${sourceFile2} successfully concatenated into ${destinationFile}`);
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
