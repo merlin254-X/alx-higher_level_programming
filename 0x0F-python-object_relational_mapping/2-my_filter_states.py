4618a5eca24ce1b5d4bb0b100410c4d15ed5b3be
merlin254-X <merlinderick8@gmail.com>
2024-03-15 11:56:43 -0700

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-""" Script that displays all values in the states table of hbtn_0e_0_usa
-    where name matches the argument """
+"""
+Script that displays all values in the states table of hbtn_0e_0_usa
+    where name matches the argument 
+"""
 
 import MySQLdb
 import sys
