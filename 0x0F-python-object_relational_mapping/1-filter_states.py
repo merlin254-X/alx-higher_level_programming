2a26514f906f855a685464c64a6d1ee7198738cc
merlin254-X <merlinderick8@gmail.com>
2024-03-15 10:51:38 -0700

    task1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,13 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
 
     # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+    db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=username,
+    passwd=password,
+    db=db_name
+    )
 
     # Create a cursor object
     cursor = db.cursor()
